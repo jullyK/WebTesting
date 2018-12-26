f2610238f50a0cd327c88166610b3cb036aa7935
Kevin <kevin.jully@free.fr>
2018-12-26 06:45:16 -0500

    getting every link related to the website from the asked url

--- spider/spider.py
+++ spider/spider.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 import requests
+import re
+import urlparse
 
 
-def request(url):
-    try:
-        return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
-        pass
+def extract_links_from(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', response.content)
 
+
+target_url = "10.0.2.9/mutillidae/"
+href_links = extract_links_from("http://" + target_url)
+for link in href_links:
+    link = urlparse.urljoin("http://" + target_url, link)
+    if target_url in link:
+        print link
