a9f7f28275e691b74dce1f1c8b9be88ce8a8cb47
Kevin <kevin.jully@free.fr>
2018-12-26 05:49:42 -0500

    adding a subdomains list file and verifying if thoses domains exists

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,5 +2,19 @@
 
 import requests
 
-url = "google.com"
-get_response = requests.get("http://" + url)
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+target_url = ".google.com"
+with open("subdomains.list", "r") as wordlist_file:
+    for line in wordlist_file:
+        line = line.strip()
+        test_url = line + target_url
+        result = request(test_url)
+        if result:
+            print("[+] Discovered subdomain -> " + test_url)
