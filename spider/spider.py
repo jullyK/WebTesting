fa4f72eda1f563d48c0f9829f45c123077b06b92
Kevin <kevin.jully@free.fr>
2018-12-26 06:20:17 -0500

    first commit of the spider project

---
+++ spider/spider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import requests
+
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
