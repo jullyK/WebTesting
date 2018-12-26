da72983608758a3913e4cfb83c5a5271ee7170f2
Kevin <kevin.jully@free.fr>
2018-12-26 07:10:26 -0500

    mapping the full website

--- spider/spider.py
+++ spider/spider.py
@@ -6,18 +6,28 @@ import urlparse
 
 
 def extract_links_from(url):
-    response = requests.get(url)
-    return re.findall('(?:href=")(.*?)"', response.content)
+    try:
+        response = requests.get(url)
+        return re.findall('(?:href=")(.*?)"', response.content)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+def crawl(url):
+    href_links = extract_links_from("http://" + url)
+    if href_links:
+        for link in href_links:
+            link = urlparse.urljoin("http://" + url, link)
+
+            if "#" in link:
+                link = link.split("#")[0]
+
+            if url in link and link not in target_links:
+                target_links.append(link)
+                print(link)
+                crawl(link)
 
 
 target_url = "10.0.2.9/mutillidae/"
 target_links = []
-
-href_links = extract_links_from("http://" + target_url)
-for link in href_links:
-    link = urlparse.urljoin("http://" + target_url, link)
-    if "#" in link:
-        link = link.split("#")[0]
-    if target_url in link and link not in target_links:
-        target_links.append(link)
-        print(link)
+crawl(target_url)
