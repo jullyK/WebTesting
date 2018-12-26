609badf5180f34184e8f57bbf418e29d39fe0409
Kevin <kevin.jully@free.fr>
2018-12-26 06:10:53 -0500

    addding dirb common.txt and adding the function to check urls

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -10,11 +10,32 @@ def request(url):
         pass
 
 
-target_url = ".google.com"
-with open("subdomains.list", "r") as wordlist_file:
-    for line in wordlist_file:
-        line = line.strip()
-        test_url = line + target_url
-        result = request(test_url)
-        if result:
-            print("[+] Discovered subdomain -> " + test_url)
+def get_subdomains(target_url):
+    results = []
+    with open("subdomains.list", "r") as wordlist_file:
+        for line in wordlist_file:
+            line = line.strip()
+            test_url = line + "." + target_url
+            result = request(test_url)
+            if result:
+                print("[+] Discovered subdomain -> " + test_url)
+                results.append(test_url)
+
+
+def get_URL(target_url):
+    results = []
+    with open("common.txt", "r") as wordlist_file:
+        for line in wordlist_file:
+            line = line.strip()
+            test_url = target_url + "/" + line
+            result = request(test_url)
+            if result:
+                print("[+] Discovered URL -> " + test_url)
+                results.append(test_url)
+
+
+#metasploitable#
+url = "10.0.2.9/mutillidae/"
+
+get_subdomains(url)
+get_URL(url)
